94e915d8ea64a62c94d0716c74fa93505f8de3a6
David <github.6v6mg@passmail.net>
2024-03-14 21:03:38 -0400

    refactor(elixir) Day 2 - Simplifying sum logic

--- apps/day_2/lib/cube_conundrum.ex
+++ apps/day_2/lib/cube_conundrum.ex
@@ -5,21 +5,29 @@ defmodule CubeConundrum do
     |> Enum.map(&String.split(&1, ":"))
     |> Enum.map(&Enum.at(&1, 1))
     |> Enum.map(&get_cubes(&1))
-    |> Enum.map(&is_game_valid(&1))
+    |> Enum.map(&is_game_valid?(&1))
     |> Enum.with_index(1)
-    |> Enum.filter(fn {k, v} -> k == true end)
-    |> Enum.map(fn {k, v} -> v end)
-    |> Enum.sum()
+    |> sum_games()
   end
 
-  defp is_game_valid([]) do
-    true
+  defp sum_games(list, count \\ 0)
+
+  defp sum_games([], count), do: count
+
+  defp sum_games([head | tail], count) do
+    if elem(head, 0) do
+      sum_games(tail, count + elem(head, 1))
+    else
+      sum_games(tail, count)
+    end
   end
 
-  defp is_game_valid(game) do
+  defp is_game_valid?([]), do: true
+
+  defp is_game_valid?(game) do
     [head | tail] = game
     if(is_grab_ok?(head)) do
-      is_game_valid(tail)
+      is_game_valid?(tail)
     else
       false
     end
